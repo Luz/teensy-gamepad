f8abf355d81635bcc8020d9bc02e506c8233e476
Raphael Nestler <raphael.nestler@gmail.com>
2017-11-27 21:39:55 +0100

    Fix key layout and optimize for wordwarvi

--- guitar.c
+++ guitar.c
@@ -4,28 +4,48 @@
 
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
 
+static const uint8_t B_MASK      = 0x01;
+static const uint8_t SELECT_MASK = 0x02;
+static const uint8_t START_MASK  = 0x04;
+static const uint8_t A_MASK      = 0x08;
+
+static const uint8_t UP_MASK     = 0x01;
+static const uint8_t DOWN_MASK   = 0x02;
+static const uint8_t LEFT_MASK   = 0x04;
+static const uint8_t RIGHT_MASK  = 0x08;
+
 int main(void) {
 	uint8_t bnew;
+	uint8_t dnew;
 	uint8_t keys[6];
 
 	CPU_PRESCALE(0); // 16 MHz clock
 	DDRB = 0x00; //pins B are inputs with pullup resistors
 	PORTB = 0xFF; //pins B are inputs with pullup resistors
+
+	// pins D are inputs with pullup resistors
+	DDRD = 0x00;
+	PORTD = 0xFF;
+
 	usb_init(); // Wait for a USB-Host
 	while(!usb_configured());
 	_delay_ms(1000); //Wait one more second for the OS to set drivers
 
 	while(1) {
-		bnew = PINB; //read port B pins
+		bnew = PINB; // read port B pins
+		dnew = PIND; // read port D pins
 
 		for(int i=0; i<6; i++) keys[i] = 0;
 
-		if (0 == (bnew & 0x01)) keys[0] = KEY_F1; //PB0 -> F1
-		if (0 == (bnew & 0x02)) keys[1] = KEY_F2; //PB1 -> F2
-		if (0 == (bnew & 0x04)) keys[2] = KEY_F3; //PB2 -> F3
-		if (0 == (bnew & 0x08)) keys[3] = KEY_F4; //PB3 -> F4
-		if (0 == (bnew & 0x80)) keys[4] = KEY_F5; //PB7 -> F5
-		if (0 == (bnew & 0x10)) keys[5] = KEY_SPACE; //PB4 -> SPACE
+		if (0 == (bnew & A_MASK))      keys[0] = KEY_SPACE;
+		if (0 == (bnew & SELECT_MASK)) keys[1] = KEY_ESC;
+		if (0 == (bnew & B_MASK))      keys[2] = KEY_B;
+		if (0 == (bnew & START_MASK))  keys[3] = KEY_Q;
+
+		if (0 == (dnew & UP_MASK))   keys[4] = KEY_K;
+		if (0 == (dnew & DOWN_MASK)) keys[4] = KEY_J;
+		if (0 == (dnew & LEFT_MASK))  keys[5] = KEY_H;
+		if (0 == (dnew & RIGHT_MASK)) keys[5] = KEY_L;
 
 		//TODO: only send if keys have changed!
 
